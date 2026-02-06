f2dd9a67d81a0891f5b09ce63e241f230f76929d
pedrodiniz00 <pedrodiniz00@gmail.com>
2026-02-06 17:45:31 -0300

    Update app_cloud.py

---
+++ app_cloud.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Teste de Vida")
+st.title("ESTOU VIVO! 🚀")
+st.write("Se você está lendo isso, o servidor e os requisitos estão certos.")
+st.write("O problema provavelmente está na conexão com o Google.")
